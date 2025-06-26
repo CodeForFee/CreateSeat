c8f2a4c0ffb233d52b1020c2a80b0ade1a45074c
NguyenNhatMinh <dienhoanguyen11@gmail.com>
2025-06-27 01:01:09 +0700

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,21 @@
+// // auth.js
+
+// // Lưu token vào localStorage
+// function saveToken(token) {
+//   localStorage.setItem("access_token", token.access_token);
+//   localStorage.setItem("refresh_token", token.refresh_token);
+// }
+
+// // Lấy token
+// function getAccessToken() {
+//   return localStorage.getItem("access_token");
+// }
+
+// // Kiểm tra đã login chưa
+// function requireLogin() {
+//   const token = getAccessToken();
+//   if (!token) {
+//     alert("Vui lòng đăng nhập trước!");
+//     window.location.href = "login.html";
+//   }
+// }
